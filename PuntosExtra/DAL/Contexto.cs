12d577bb47a2cf2a3e6a87b46b4c035a05445fc8
Emminton Manuel Ure�a Santana <emminton_urena@ucne.edu.do>
2020-03-06 21:06:06 -0400

    Creando la DaL

---
+++ PuntosExtra/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PuntosExtra.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PuntosExtra.DAL 
+{
+       public class Contexto : DbContext
+         {
+            public DbSet<Personas> Persona { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+            {
+                optionBuilder.UseSqlite(@"Data Source = Persona.db");
+            }
+       }
+}
