6846fc20c3314ca9ad76c3cf5638dd1a926b896d
Emminton Manuel Ure�a Santana <emminton_urena@ucne.edu.do>
2020-03-06 21:16:26 -0400

    Creando el Registro Consulta

---
+++ PuntosExtra/UI/Consultas/Consultas.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PuntosExtra.UI.Consultas
+{
+    /// <summary>
+    /// Interaction logic for Consultas.xaml
+    /// </summary>
+    public partial class Consultas : Window
+    {
+        public Consultas()
+        {
+            InitializeComponent();
+        }
+    }
+}
